d2a6b37bd2e49fd5a6a4a14c6222a92603bc433e
Minhal Zaidi <minhal.zaidi@student.uts.edu.au>
2021-09-20 15:46:58 +1000

    Track caret position of controlled input

--- src/components/List/Input.js
+++ src/components/List/Input.js
@@ -1,21 +1,32 @@
 import { doc, updateDoc } from "firebase/firestore";
 import { useFirestore } from "reactfire";
 import { useRouter } from "next/router";
+import { useState, useRef, useEffect } from "react";
 
 function Input({ name, value, placeholder, completed, larger, mb }) {
+  const [caretPosition, setCaretPosition] = useState(null);
   const firestore = useFirestore();
   const router = useRouter();
   const { id } = router.query;
+  const ref = useRef(null);
+
   function updateField(e) {
-    const { value } = e.target;
+    const { value, selectionStart } = e.target;
+    setCaretPosition(selectionStart);
     const ref = doc(firestore, "lists", id);
     updateDoc(ref, {
       [name]: value,
     });
   }
 
+  useEffect(() => {
+    const input = ref.current;
+    if (input) input.setSelectionRange(caretPosition, caretPosition);
+  }, [ref, caretPosition, value]);
+
   return (
     <input
+      ref={ref}
       onChange={updateField}
       className={`block py-3 px-3 lg:px-5 w-full ${
         larger ? "text-xl lg:text-2xl" : "lg:text-lg"
