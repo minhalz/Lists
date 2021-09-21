ee7f77867758702f914d0961e1c7fb05b5ec58fc
Minhal Zaidi <minhal.zaidi@student.uts.edu.au>
2021-09-21 20:09:09 +1000

    Fixed predictive text issues

--- src/components/List/Input.js
+++ src/components/List/Input.js
@@ -4,14 +4,18 @@ import { useRouter } from "next/router";
 import { useState, useRef, useEffect } from "react";
 
 function Input({ name, value, placeholder, completed, larger, mb }) {
-  const [caretPosition, setCaretPosition] = useState(null);
+  const [caretPosition, setCaretPosition] = useState(0);
+  const [inputVal, setInputVal] = useState("");
   const firestore = useFirestore();
   const router = useRouter();
   const { id } = router.query;
   const ref = useRef(null);
+  const input = ref.current;
+  if (input) input.setSelectionRange(caretPosition, caretPosition);
 
   function updateField(e) {
     const { value, selectionStart } = e.target;
+    setInputVal(value);
     setCaretPosition(selectionStart);
     const ref = doc(firestore, "lists", id);
     updateDoc(ref, {
@@ -20,14 +24,13 @@ function Input({ name, value, placeholder, completed, larger, mb }) {
   }
 
   useEffect(() => {
-    const input = ref.current;
-    if (input) input.setSelectionRange(caretPosition, caretPosition);
-  }, [ref, caretPosition, value]);
+    setInputVal(value);
+  }, [value]);
 
   return (
     <input
       ref={ref}
-      onInput={updateField}
+      onChange={updateField}
       className={`block py-3 px-3 lg:px-5 w-full ${
         larger ? "text-xl lg:text-2xl" : "lg:text-lg"
       } leading-tight text-white align-middle bg-primary-very-dark-desaturated-blue focus-visible:outline-none caret-primary${
@@ -35,7 +38,7 @@ function Input({ name, value, placeholder, completed, larger, mb }) {
           ? " line-through text-primary-very-dark-gray-blue placeholder-primary-very-dark-gray-blue"
           : ""
       }`}
-      value={value}
+      value={inputVal}
       placeholder={placeholder}
       disabled={completed}
       spellCheck="true"
