752ecd84980ea44dee088d275dfcaacd8a10df53
Minhal Zaidi <minhal.zaidi@student.uts.edu.au>
2022-04-22 19:15:16 +1000

    Added privacy policy

--- src/components/SiteLayout.js
+++ src/components/SiteLayout.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Link from "next/link";
 
 function SiteLayout({ children }) {
   return (
@@ -7,6 +8,11 @@ function SiteLayout({ children }) {
       <main className="relative px-3 mx-auto -mt-40 lg:-mt-36 max-w-[600px]">
         {children}
       </main>
+      <div className="flex relative justify-center py-4">
+        <Link href="/privacy-policy">
+          <a className="text-white hover:underline">Privacy Policy</a>
+        </Link>
+      </div>
     </div>
   );
 }
